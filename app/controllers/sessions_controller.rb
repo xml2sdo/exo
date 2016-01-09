2b3b1b054f57384b97b846f1cb8ba84ee4c1e22a
Philippe RASSINOUX <rassinoux.philippe@gmail.com>
2016-01-09 16:48:26 +0100

    Déconnexion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user=User.find_by(login: params[:session][:login])
-  	p user
+  	user=User.find_by(login: params[:session][:login].downcase)
   	if user && user.pwd == params[:session][:password]
-  		p "="*100
-  		session[:user_id]= user.id #log_in user
-  		redirect_to user
+      log_in user
+      redirect_to user
   	else
-  		p ". "*100
   		flash.now[:danger] = 'Merci de ré-essayer'
 	  	render 'new'
 	  end
   end
+
+  def destroy
+      log_out
+      redirect_to root_url
+  end
+
+private
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
+  get     'login' => 'sessions#new'
+  post    'login' => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
